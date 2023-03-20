18bcaec0db9936f4b616ca48b2212c41e450d48f
cecevl <cecevl070@gmail.com>
2023-03-20 07:48:04 +0300

    init

---
+++ mainCV/main.py
@@ -0,0 +1,29 @@
+import telegram
+import asyncio
+import pyperclip
+
+# Replace YOUR_TOKEN with your bot token
+bot = telegram.Bot(token=TOKEN)
+last_copied_message = ""
+
+async def main():
+    last_copied_message = ""
+    while True:
+        chat_id, message = await get_last_message()
+        if message and message != last_copied_message:
+            # Copy the text of the message to the clipboard if it's different from the last copied message
+            pyperclip.copy(message)
+            last_copied_message = message
+        await asyncio.sleep(1) # Add a small delay before the next check
+
+
+async def get_last_message():
+    async with bot:
+        updates = await bot.get_updates()
+        if not updates:
+            return None, None
+        message = updates[-1].message.text
+        chat_id = updates[-1].message.chat_id
+        return chat_id, message
+
+asyncio.run(main())
